e3c2ae9fc45ac5a6f00f313e74ef6813e1d09248
Saloni Kathuria <kathuria.saloni29@gmail.com>
2018-09-16 23:47:11 +0530

    user model added

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+let UserSchema = mongoose.Schema({
+    username : {
+        type : String
+    },
+    email : {
+        type : String
+    },
+    password : {
+        type : String
+    }
+    
+})
+
+let User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.registerUser = function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+	        newUser.password = hash;
+	        newUser.save(callback);
+	    });
+	});
+}
+
+module.exports.getUserByEmail = function(email, callback){
+	var query = {email: email};
+	User.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
