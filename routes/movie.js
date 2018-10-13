9b5f0deb0f7fdbaceea18b07577e344344faf779
Saloni Kathuria <kathuria.saloni29@gmail.com>
2018-10-14 01:15:35 +0530

    GET /browse route added

--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const {apiKey} = require('../config/settings');
+const {apiKey, base_uri} = require('../config/settings');
+const request = require('request');
 
 router.get('/browse', (req, res) => {
-    res.render('browse' , { title : 'MovieDB'});
+
+    //get movies
+    request(base_uri + '/3/discover/movie?api_key=' + apiKey , { json : true} , (err, resp, body) => {
+        if (err) { return console.log(err); }
+        
+        console.log(body);
+        // let body = {};
+        // body['results'] = [1,2,3,4,5,6,7,8,9,10, 11, 12,13,14,15,16,17,18,19,20]
+
+        res.render('browse' , { title : 'MovieDB' , 'data' : body.results , 'page' : body.page });
+   })
+    
 })
 
 module.exports = router;
