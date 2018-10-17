4fcacb03c3d2bc1ae5e8dc33c4f35e82f3cdf699
Saloni Kathuria <kathuria.saloni29@gmail.com>
2018-10-17 10:27:08 +0530

    changes in /browse route

--- routes/movie.js
+++ routes/movie.js
@@ -6,14 +6,15 @@ const request = require('request');
 router.get('/browse', (req, res) => {
 
     //get movies
-    request(base_uri + '/3/discover/movie?api_key=' + apiKey , { json : true} , (err, resp, body) => {
+    const url = base_uri + '/3/discover/movie?api_key=' + apiKey + "&language=en-US&sort_by=popularity.desc";
+    request(url  , { json : true} , (err, resp, body) => {
         if (err) { return console.log(err); }
         
         console.log(body);
         // let body = {};
         // body['results'] = [1,2,3,4,5,6,7,8,9,10, 11, 12,13,14,15,16,17,18,19,20]
 
-        res.render('browse' , { title : 'MovieDB' , 'data' : body.results , 'page' : body.page });
+        res.render('browse' , { title : 'MovieDB' , 'data' : body.results , 'page' : body.page , 'url' : url });
    })
     
 })
