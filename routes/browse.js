bb131744abb1e70425dcba8fcfac67bbb9cdb148
Saloni Kathuria <kathuria.saloni29@gmail.com>
2019-01-10 02:11:56 +0530

    methods added to fetch data

---
+++ routes/browse.js
@@ -0,0 +1,54 @@
+const {apiKey, base_uri} = require('../config/settings');
+const request = require('request');
+
+const getNowPlaying = (callback) => {
+    const url = base_uri + '/3/movie/now_playing?api_key=' + apiKey + "&language=en-US";
+    request(url  , { json : true} , (err, resp, body) => {
+        if(err)
+            return callback(null, [])
+
+        if(body)
+            callback(null, body.results);
+    })
+}
+
+const getUpcoming = (callback) => {
+    const url = base_uri + '/3/movie/upcoming?api_key=' + apiKey + "&language=en-US";
+    request(url  , { json : true} , (err, resp, body) => {
+        if(err)
+            return callback([])
+
+        if(body)
+            callback(null, body.results);
+    })
+}
+
+
+const getPopular = (callback) => {
+    const url = base_uri + '/3/movie/popular?api_key=' + apiKey + "&language=en-US";
+    request(url  , { json : true} , (err, resp, body) => {
+        if(err)
+            return callback(null, [])
+
+        if(body)
+            callback(null, body.results);
+    })
+}
+
+const getTopRated = (callback) => {
+    const url = base_uri + '/3/movie/top_rated?api_key=' + apiKey + "&language=en-US";
+    request(url  , { json : true} , (err, resp, body) => {
+        if(err)
+            return callback(null, [])
+
+        if(body)
+            callback(null, body.results);
+    })
+}
+
+module.exports= {
+    getNowPlaying,
+    getUpcoming,
+    getPopular,
+    getTopRated
+}
